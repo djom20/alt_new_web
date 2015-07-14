5b506d957c5085d70056ed965c7377c9172f277a
Ing. Jonathan Olier <djom202@gmail.com>
2015-07-14 13:47:07 -0500

    Change Style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 $(function(){
 	// Your code here
+	nav();
 });
+
+function nav(){
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 70){
+			console.log("3");
+		}else{
+			$("nav").animate({
+				duration: 100,
+				easing: 'swing',
+				step: function(val) {
+					console.log("step");
+				},
+				complete: function() {
+					console.log("finish");
+    			}
+			});
+		}
+	});
+}
